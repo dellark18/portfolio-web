3959a72aa5d076acd9bcc2b36aeb065610ee67e9
dellark18 <ajayibamidele60@gmail.com>
2022-06-21 04:13:55 +0100

    final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,34 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+// Home Scroll Section
+sr.reveal('.home-text',{})
+sr.reveal('.btn-light',{delay: 200})
+sr.reveal('.home-circle',{delay: 400})
+sr.reveal('.home-social-icon',{interval: 200})
+
+// About Scroll Section
+sr.reveal('.about-img',{})
+sr.reveal('.about-head',{delay: 200})
+sr.reveal('.about-paragraph',{delay: 400})
+
+// Skills Scroll Section
+sr.reveal('.skills-img',{})
+sr.reveal('.skills-head',{delay: 200})
+sr.reveal('.skills-paragraph',{delay: 400})
+sr.reveal('.skills-data',{interval: 200})
+
+// Work Scroll Section
+sr.reveal('.work-img',{interval: 200})
+
+// Contact Scroll Section
+sr.reveal('.contactForm',{interval: 200})
